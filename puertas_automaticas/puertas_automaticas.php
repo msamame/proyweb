cf19535ff06fe98634ff65f8d9d95b622fb71ca0
msamame <msamame@hotmail.com>
2020-04-13 16:28:57 -0500

    Actualizacion para los link del proyecto web

--- puertas_automaticas/puertas_automaticas.php
+++ puertas_automaticas/puertas_automaticas.php
@@ -5,7 +5,8 @@
 <?php require_once('../libs/menu_horizontal.php'); ?>
 <!-- Cuerpo de link -->
 <div class="encabezado_link">Puertas automáticas<?php require_once('../libs/rs_encabezado_link.php');?></div>
-<div class="body_link">cuerpo de pagina</div>
+<div class="body_link_centro_pagina">
+contenido puertas automáticas</div>
 
 
 
