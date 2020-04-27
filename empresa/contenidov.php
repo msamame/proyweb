4cec47d6d2f77990b4b6b253d674d3ef6801bd6d
msamame <msamame@hotmail.com>
2020-04-27 10:38:09 -0500

    Avances pagina web

--- empresa/contenidov.php
+++ empresa/contenidov.php
@@ -1,32 +1,33 @@
-<table width="200" border="0">
+<table width="800" height="800" border="0">
   <tr>
-    <td><iframe width="200" height="180" src="../videos/VID_20180314_082801.mp4?autoplay=0" frameborder="0" allowfullscreen scrolling="no"></iframe></td>
-    <td>&nbsp;</td>
-    <td>&nbsp;</td>
-    <td>&nbsp;</td>
+    <td><video width="150" height="130" controls src="../videos/VID_001.mp4" frameborder="1" allowfullscreen scrolling="no" type="video/mp4"></video></td>
+
+    <td><video width="150" height="130" controls src="../videos/VID_002.mp4" frameborder="0" allowfullscreen scrolling="no" type="video/mp4"></video></td>
+    <td><video width="150" height="130" controls src="../videos/VID_003.mp4" frameborder="0" allowfullscreen scrolling="no" type="video/mp4"></video></td>
+    <td><video width="150" height="130" controls src="../videos/VID_004.mp4" frameborder="0" allowfullscreen scrolling="no" type="video/mp4"></video></td>
   </tr>
   <tr>
-    <td>&nbsp;</td>
-    <td>&nbsp;</td>
-    <td>&nbsp;</td>
-    <td>&nbsp;</td>
+    <td><video width="150" height="130" controls src="../videos/VID_005.mp4" frameborder="0" allowfullscreen scrolling="no" type="video/mp4"></video></td>
+    <td><video width="150" height="130" controls src="../videos/VID_006.mp4" frameborder="0" allowfullscreen scrolling="no" type="video/mp4"></video></td>
+    <td><video width="150" height="130" controls src="../videos/VID_007.mp4" frameborder="0" allowfullscreen scrolling="no" type="video/mp4"></video></td>
+    <td><video width="150" height="130" controls src="../videos/VID_008.mp4" frameborder="0" allowfullscreen scrolling="no" type="video/mp4"></video></td>
   </tr>
   <tr>
-    <td>&nbsp;</td>
-    <td>&nbsp;</td>
-    <td>&nbsp;</td>
-    <td>&nbsp;</td>
+    <td><video width="150" height="130" controls src="../videos/VID_009.mp4" frameborder="0" allowfullscreen scrolling="no" type="video/mp4"></video></td>
+    <td><video width="150" height="130" controls src="../videos/VID_010.mp4" frameborder="0" allowfullscreen scrolling="no" type="video/mp4"></video></td>
+    <td><video width="150" height="130" controls src="../videos/VID_011.mp4" frameborder="0" allowfullscreen scrolling="no" type="video/mp4"></video></td>
+    <td><video width="150" height="130" controls src="../videos/VID_012.mp4" frameborder="0" allowfullscreen scrolling="no" type="video/mp4"></video></td>
   </tr>
   <tr>
-    <td>&nbsp;</td>
-    <td>&nbsp;</td>
-    <td>&nbsp;</td>
-    <td>&nbsp;</td>
+    <td><video width="150" height="130" controls src="../videos/VID_013.mp4" frameborder="0" allowfullscreen scrolling="no" type="video/mp4"></video></td>
+    <td><video width="150" height="130" controls src="../videos/VID_014.mp4" frameborder="0" allowfullscreen scrolling="no" type="video/mp4"></video></td>
+    <td><video width="150" height="130" controls src="../videos/VID_015.mp4" frameborder="0" allowfullscreen scrolling="no" type="video/mp4"></video></td>
+    <td><video width="150" height="130" controls src="../videos/VID_016.mp4" frameborder="0" allowfullscreen scrolling="no" type="video/mp4"></video></td>
   </tr>
   <tr>
-    <td>&nbsp;</td>
-    <td>&nbsp;</td>
-    <td>&nbsp;</td>
+    <td><video width="150" height="130" controls src="../videos/VID_017.mp4" frameborder="0" allowfullscreen scrolling="no" type="video/mp4"></video></td>
+    <td><video width="150" height="130" controls src="../videos/VID_018.mp4" frameborder="0" allowfullscreen scrolling="no" type="video/mp4"></video></td>
+    <td><video width="150" height="130" controls src="../videos/VID_019.mp4" frameborder="0" allowfullscreen scrolling="no" type="video/mp4"></video></td>
     <td>&nbsp;</td>
   </tr>
 </table>
